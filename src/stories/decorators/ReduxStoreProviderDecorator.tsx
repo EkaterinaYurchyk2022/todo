70c4ea786e44846dc259f69e81388a0b97bd1ede
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-08-18 23:21:04 +0300

    redux store provider decorator

--- src/stories/decorators/ReduxStoreProviderDecorator.tsx
+++ src/stories/decorators/ReduxStoreProviderDecorator.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import {Provider} from 'react-redux'
-import {applyMiddleware, combineReducers, createStore} from 'redux'
-import {tasksReducer} from '../../features/TodolistsList/tasks-reducer'
-import {todolistsReducer} from '../../features/TodolistsList/todolists-reducer'
+import {combineReducers} from 'redux'
+import {tasksReducer} from '../../features/TodolistsList'
+import {todolistsReducer} from '../../features/TodolistsList'
 import {v1} from 'uuid'
-import {AppRootStateType, RootReducerType} from '../../app/store'
-import {TaskPriorities, TaskStatuses} from '../../api/todolists-api'
-import {appReducer} from '../../app/app-reducer'
+import {appReducer} from '../../features/Application'
 import thunkMiddleware from 'redux-thunk'
-import {authReducer} from '../../features/Auth/auth-reducer'
+import {authReducer} from '../../features/Auth'
 import {configureStore} from '@reduxjs/toolkit'
-import {BrowserRouter, HashRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
+import {AppRootStateType, RootReducerType} from '../../utils/types'
+import {TaskPriorities, TaskStatuses} from '../../api/types'
 
 const rootReducer: RootReducerType = combineReducers({
     tasks: tasksReducer,
@@ -21,7 +21,7 @@ const rootReducer: RootReducerType = combineReducers({
 
 const initialGlobalState: AppRootStateType = {
     todolists: [
-        {id: "todolistId1", title: "What to learn", filter: "all", entityStatus: 'idle', addedDate: '', order: 0},
+        {id: "todolistId1", title: "What to learn What to learn What to learn What to learn", filter: "all", entityStatus: 'idle', addedDate: '', order: 0},
         {id: "todolistId2", title: "What to buy", filter: "all", entityStatus: 'loading', addedDate: '', order: 0}
     ] ,
     tasks: {
