632b3189593c5fa3d69bfc55a9b05dadc0899ad0
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-08-18 23:23:57 +0300

    types in utils

---
+++ src/utils/types.ts
@@ -0,0 +1,10 @@
+import {rootReducer, store} from '../app/store'
+import {FieldErrorType} from '../api/types'
+
+
+// redux common types
+export type RootReducerType = typeof rootReducer
+// определить автоматически тип всего объекта состояния
+export type AppRootStateType = ReturnType<RootReducerType>
+export type AppDispatchType = typeof store.dispatch
+export type ThunkError = { rejectValue: { errors: Array<string>, fieldsErrors?: Array<FieldErrorType> } }
