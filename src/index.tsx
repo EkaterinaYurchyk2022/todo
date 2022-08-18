bdb49c17f00564caad1871f749e73e0ae63179f5
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2022-08-18 23:25:09 +0300

    index root folder

--- src/index.tsx
+++ src/index.tsx
@@ -10,11 +10,11 @@ import {BrowserRouter} from 'react-router-dom'
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <App/>
+            <App />
         </BrowserRouter>
     </Provider>, document.getElementById('root'))
 
-// If you want your app to work offline and load faster, you can change
+// If you want your appActions to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister()
